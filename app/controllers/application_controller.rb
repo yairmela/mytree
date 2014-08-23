a792da7a60b41954a2baa58ae323f5609d9d3e50
yair melamed <yairmela@gmail.com>
2014-08-23 14:15:43 +0300

    return ruby ver

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   protect_from_forgery with: :exception
 
-  after_filter :set_csrf_cookie_for_ng
+  # after_filter :set_csrf_cookie_for_ng
 
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
