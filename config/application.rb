d2eb3a045fc9318ffc0a07b2bea56e252a50c494
yair melamed <yairmela@gmail.com>
2014-08-24 22:47:52 +0300

    try fix

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module Mytree
   class Application < Rails::Application
     I18n.enforce_available_locales = true
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Mytree::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -44,10 +44,10 @@ Mytree::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
-  config.assets.debug = true
-  config.active_record.migration_error = :page_load
-  config.active_support.deprecation = :log
-  config.action_mailer.raise_delivery_errors = false
+  # config.assets.debug = true
+  # config.active_record.migration_error = :page_load
+  # config.active_support.deprecation = :log
+  # config.action_mailer.raise_delivery_errors = false
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
